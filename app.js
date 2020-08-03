3d11e1025febdc25285f11597f4e29639f015fa9
Tushar Sharma <63466210+Tusharmaa@users.noreply.github.com>
2020-08-04 03:36:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -18,32 +18,16 @@ const game = () => {
         const options = document.querySelectorAll(".options button");
         const playerHand = document.querySelector(".player-hand");
         const computerHand = document.querySelector(".computer-hand");
-        const hands = document.querySelectorAll(".hands img");
 
-        hands.forEach(hand => {
-            hand.addEventListener("animationend", function () {
-                this.style.animation = "";
-            });
-        });
-        //Computer Options
-        const computerOptions = ["rock", "paper", "scissors"];
-
+        const computerOptions = ["Rock", "Paper", "Scissors"];
         options.forEach(option => {
             option.addEventListener("click", function () {
-                //Computer Choice
                 const computerNumber = Math.floor(Math.random() * 3);
                 const computerChoice = computerOptions[computerNumber];
-
-                setTimeout(() => {
-                    //Here is where we call compare hands
-                    compareHands(this.textContent, computerChoice);
-                    //Update Images
-                    playerHand.src = `./assets/${this.textContent}.png`;
-                    computerHand.src = `./assets/${computerChoice}.png`;
-                }, 2000);
-                //Animation
-                playerHand.style.animation = "shakePlayer 2s ease";
-                computerHand.style.animation = "shakeComputer 2s ease";
+                compareHands(this.textContent, computerChoice);
+                //Update Images
+                playerHand.src = `./assets/${this.textContent}.png`;
+                computerHand.src = `./assets/${computerChoice}.png`;
             });
         });
     };
@@ -58,59 +42,23 @@ const game = () => {
     const compareHands = (playerChoice, computerChoice) => {
         //Update Text
         const winner = document.querySelector(".winner");
-        //Checking for a tie
+
+        //Check if tie or not and who wins
         if (playerChoice === computerChoice) {
             winner.textContent = "It is a tie";
-            return;
-        }
-        //Check for Rock
-        if (playerChoice === "rock") {
-            if (computerChoice === "scissors") {
-                winner.textContent = "Player Wins";
-                pScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Computer Wins";
-                cScore++;
-                updateScore();
-                return;
-            }
-        }
-        //Check for Paper
-        if (playerChoice === "paper") {
-            if (computerChoice === "scissors") {
-                winner.textContent = "Computer Wins";
-                cScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Player Wins";
-                pScore++;
-                updateScore();
-                return;
-            }
-        }
-        //Check for Scissors
-        if (playerChoice === "scissors") {
-            if (computerChoice === "rock") {
-                winner.textContent = "Computer Wins";
-                cScore++;
-                updateScore();
-                return;
-            } else {
-                winner.textContent = "Player Wins";
-                pScore++;
-                updateScore();
-                return;
-            }
+        } else if (playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Paper" && computerChoice === "Rock" || playerChoice === "Scissors" && computerChoice === "Paper") {
+            winner.textContent = "Player Wins."
+            pScore++;
+            updateScore();
+        } else {
+            winner.textContent = "Computer Wins";
+            cScore++;
+            updateScore();
         }
     };
-
     //Is call all the inner function
     startGame();
     playMatch();
 };
-
 //start the game function
 game();
